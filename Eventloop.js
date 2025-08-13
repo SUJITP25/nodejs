3c96c8afe31dbe3e7f968235666c22e00dc256aa
SUJITP25 <sujitpatel1111@gmail.com>
2025-08-13 19:28:26 +0530

    frst commit

--- Eventloop.js
+++ Eventloop.js
@@ -1,6 +1,7 @@
 // Javascript is synchronous, blocking and single-theraded language.
 // To make aync programming possible, we need the help of libuv.
 
+
 // Asyncronous Code Execution
 // memory heap - All declared variables and functions resides here. 
 // callstack - Whenever we execure code, functions are pushed into the call stack.
@@ -8,11 +9,23 @@
 // fs.readFile() - async function handle by libuv by native async mechaism, Thread pool.
 
 
+// import fs from "fs"
+// console.log("first")
+// fs.readFile('data.txt',()=>{
+//     console.log("Second")
+// })
+// console.log("third")
+// first,third and then second will be executed.
+
+
+
 // Synchronous Code Execution
 // Call Stack- global()
 // console.log("first")
 // console.log("second")
 // console.log("third")
-
+// First, second and third will be print on console.
+// After execution done then global() function will also be removed from stack. 
+// 
 
 
