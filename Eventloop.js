d730779a24cefb7677b6d6c8e0ad0495ca2c89a4
SUJITP25 <sujitpatel1111@gmail.com>
2025-08-13 19:21:22 +0530

    frst commit

--- Eventloop.js
+++ Eventloop.js
@@ -2,3 +2,5 @@
 // To make aync programming possible, we need the help of libuv.
 // memory heap - All declared variables and functions resides here. 
 // callstack - Whenever we execure code, functions are pushed into the call stack.
+// Execution is done in LIFO manner, lastInFirstOut, stack Data Structure. 
+// fs.readFile() - async function handle by libuv by native async mechaism, Thread pool.
