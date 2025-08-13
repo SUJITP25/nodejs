adec0cfb4d469903992e540c57ed930e9da65aff
SUJITP25 <sujitpatel1111@gmail.com>
2025-08-13 19:19:03 +0530

    frst commit

--- Eventloop.js
+++ Eventloop.js
@@ -1,3 +1,4 @@
 // Javascript is synchronous, blocking and single-theraded language.
 // To make aync programming possible, we need the help of libuv.
-// 
+// memory heap - All declared variables and functions resides here. 
+// callstack - Whenever we execure code, functions are pushed into the call stack.
